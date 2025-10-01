1b2d09960478385e115f7e6a6ead1aefc27c66bf
Necromunger <lil_tommy_tom@hotmail.com>
2025-10-01 13:14:20 +1000

    Initial commit, wip

---
+++ PlantBasedFat/PlantBasedFatModSystem.cs
@@ -0,0 +1,9 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace PlantBasedFat
+{
+    public class PlantBasedFatModSystem : ModSystem { }
+}
